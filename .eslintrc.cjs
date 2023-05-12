817976401b40dd9379df33e2fdbc9a028a85c809
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-11 23:13:12 -0600

    Config eslint for this project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,25 @@
-/* eslint-env node */
-
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/react-in-jsx-scope": "off",
+  },
+};
