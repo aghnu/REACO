9b1d454011d4b31dcf2d898fa81b97fd2f74f562
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-11 23:20:39 -0600

    Clean up init code

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+`;
+
+export default GlobalStyle;
