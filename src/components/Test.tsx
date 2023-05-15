eccbb1564a06bfbe4f0aaf54dfdea9cf37b04dfb
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-14 22:45:11 -0600

    Add styled to test demo

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,14 +1,19 @@
 import { useSelector } from 'react-redux';
 import { selectOrderedDisplayStateItemsTopOrder } from '../store/displaySlice';
+import styled from 'styled-components';
+
+const TestStyled = styled.div`
+  color: red;
+`;
 
 const Test = ({ children = undefined }: { children?: React.ReactNode }) => {
   const order = useSelector(selectOrderedDisplayStateItemsTopOrder);
 
   return (
-    <div>
-      <p>{order}</p>
+    <TestStyled>
+      <p className="Test__text">{order}</p>
       {children}
-    </div>
+    </TestStyled>
   );
 };
 
