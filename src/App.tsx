28eba9b492356ebc7b19442bf1b96e4e89978df0
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-21 12:14:48 -0600

    Remove redux from the code base
    - redux is too much for this project, it is not the best choice
    - a lot of feature provided by redux is never going to be useful for this project
    - consider switching to a simpler global state management solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,7 @@
-import { useCallback } from 'react';
-import Test from './components/Test';
-import { type DisplayItem } from './types/DisplayTypes';
-import { v4 as uuid } from 'uuid';
-import { useAppDispatch, useAppSelector } from './store';
-
-import {
-  addDisplayItem,
-  removeDisplayItem,
-  selectOrderedDisplayStateItemsTop,
-  selectGetDisplayItem,
-} from './store/displaySlice';
-
 const App = () => {
-  const dispatch = useAppDispatch();
-  const topDisplayStateItem = useAppSelector(selectOrderedDisplayStateItemsTop);
-  const getCurrentTopDisplayItem = useAppSelector(selectGetDisplayItem);
-  // const [ topDisplayItemText, setTopDisplayItemText ] = useState('');
-
-  const currentTopDisplayItem =
-    topDisplayStateItem !== null
-      ? getCurrentTopDisplayItem(topDisplayStateItem.item.id)?.text ?? 'no data'
-      : ' no data 2';
-
-  const addTestingDisplayItem = useCallback(() => {
-    const displayItem: DisplayItem = {
-      id: uuid(),
-      type: 'test',
-      text: 'test',
-    };
-
-    dispatch(addDisplayItem(displayItem));
-  }, [dispatch]);
-
-  const removeTestingDisplayItemTop = useCallback(() => {
-    if (topDisplayStateItem !== null) {
-      dispatch(removeDisplayItem(topDisplayStateItem.item.id));
-    }
-  }, [dispatch, topDisplayStateItem]);
-
   return (
     <>
-      <button onClick={addTestingDisplayItem}>Add store item</button>
-      <button onClick={removeTestingDisplayItemTop}>
-        Delete top store item
-      </button>
-      <p>{currentTopDisplayItem}</p>
-      <Test></Test>
+      <p>Test</p>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import store from './store';
-import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <GlobalStyle />
-      <App />
-    </Provider>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
