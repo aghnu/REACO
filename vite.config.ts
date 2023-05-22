5a38aa8e760c1dea6ae035d9d95debbca8a8cc78
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-22 11:49:48 -0600

    Update path alias

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@styles': path.resolve(__dirname, './src/styles'),
       '@store': path.resolve(__dirname, './src/store'),
