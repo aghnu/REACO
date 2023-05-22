80d3af465d9d027f026657e78221f7d14bfe809e
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-21 22:52:07 -0600

    Remove tailwind replace it with css module
    - add sass

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,21 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@types': path.resolve(__dirname, './src/types'),
     },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@import '@styles/global.scss';",
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
   },
 });
