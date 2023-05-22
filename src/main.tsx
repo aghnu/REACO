6c2d4f8f5f9f436128b656f7d00660e44047ce82
Gengyuan Huang <gengyuan@ualberta.ca>
2023-05-22 10:44:53 -0600

    Use store from ./store for the react tree

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { enableMapSet } from 'immer';
 import { Provider } from 'jotai';
+import store from './store';
 import './main.scss';
 
 enableMapSet();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
