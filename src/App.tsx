d2b944911ea42ad5f03a5b7d7742435baeb9673e
Gengyuan Huang <gengyuan@ualberta.ca>
2023-06-04 10:25:17 -0600

    Move state provider from main.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import ConsolePage from '@pages/ConsolePage';
 import styles from '@styles/modules/wrapper.module.scss';
 import initApplicationLogic from '@applications/logics';
+import store from '@/store';
+import { Provider } from 'jotai';
 
 initApplicationLogic();
 
 const App = () => {
   return (
-    <div className={styles['container-page']}>
-      <ConsolePage />
-    </div>
+    <Provider store={store}>
+      <div className={styles['container-page']}>
+        <ConsolePage />
+      </div>
+    </Provider>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { enableMapSet } from 'immer';
-import { Provider } from 'jotai';
-import store from './store';
 import './main.scss';
 
 enableMapSet();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
