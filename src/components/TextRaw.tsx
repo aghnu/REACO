cac5072fffb022c7c3d08568e923d62f41076e4f
Gengyuan Huang <gengyuan@ualberta.ca>
2023-06-10 23:02:59 -0600

    Refactor text raw to use no break space
    - use u00a0

--- src/components/TextRaw.tsx
+++ src/components/TextRaw.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 const TextRaw = ({
   text,
@@ -9,43 +9,9 @@ const TextRaw = ({
   type?: 'span' | 'p';
   className?: string | undefined;
 }) => {
-  const inputChunks = useMemo(() => {
-    if (text === '') return [];
+  const processedText = useMemo(() => text.replaceAll(' ', '\u00a0'), [text]);
 
-    const textSplits = text.split(' ');
-    const chunks: string[] = [];
-    for (let i = 0; i < textSplits.length; i++) {
-      if (textSplits[i] !== '') chunks.push(textSplits[i]);
-      if (i + 1 < textSplits.length) chunks.push(' ');
-    }
-
-    return chunks;
-  }, [text]);
-
-  const innerContent = useMemo(
-    () => (
-      <>
-        {inputChunks.map((chunk, index) =>
-          chunk === ' ' ? (
-            <span key={index}>&nbsp;</span>
-          ) : (
-            <span key={index}>{chunk}</span>
-          )
-        )}
-      </>
-    ),
-    [inputChunks]
-  );
-
-  return (
-    <>
-      {type === 'p' ? (
-        <p className={className}>{innerContent}</p>
-      ) : (
-        <span className={className}>{innerContent}</span>
-      )}
-    </>
-  );
+  return <>{React.createElement(type, { className }, processedText)}</>;
 };
 
 export default TextRaw;
