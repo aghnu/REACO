55002379c1c69ba703f463e67d094d90a2011242
Gengyuan Huang <gengyuan@ualberta.ca>
2023-06-15 02:01:13 -0600

    Update theme color

--- src/config/globalStyle.tsx
+++ src/config/globalStyle.tsx
@@ -14,7 +14,7 @@ export const defaultGlobalStyleColorLight: GlobalStyleColor = {
   '--color-plain': '#002d8b',
   '--color-focus': '#ff0c00',
   '--color-desc': '#00393d',
-  '--color-neon': '#f7f054',
+  '--color-neon': '#ff7f7f',
   '--color-warn': '#b70c0f',
   '--color-calm': '#005fd7',
   '--color-background': '#ffeeee',
