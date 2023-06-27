69b65bdebab5e3b3456809861f19f04c7b149362
Gengyuan Huang <gengyuan@ualberta.ca>
2023-06-26 22:56:04 -0600

    Update dark mode color palette

--- src/config/globalStyle.tsx
+++ src/config/globalStyle.tsx
@@ -1,13 +1,13 @@
 import type { GlobalStyleColor } from '@type/GlobalStyleTypes';
 
 export const defaultGlobalStyleColorDark: GlobalStyleColor = {
-  '--color-plain': '#94a3c2',
-  '--color-focus': '#f59879',
-  '--color-desc': '#1ca0a9',
+  '--color-plain': '#8da1be',
+  '--color-focus': '#ffb7b7',
+  '--color-desc': '#679194',
   '--color-neon': '#f7f054',
   '--color-warn': '#b70c0f',
   '--color-calm': '#005fd7',
-  '--color-background': '#000c18',
+  '--color-background': '#02131e',
 };
 
 export const defaultGlobalStyleColorLight: GlobalStyleColor = {
