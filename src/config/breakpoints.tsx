6a7dd91c4bbbcdf1d2d7f72fa673dd8fc6df5cf0
Gengyuan Huang <gengyuan@ualberta.ca>
2023-07-02 22:58:19 -0600

    Add new breakpoint

--- src/config/breakpoints.tsx
+++ src/config/breakpoints.tsx
@@ -3,4 +3,5 @@ import { type Breakpoints } from '@type/BreakpointTypes';
 export const breakpoints: Breakpoints = {
   '--bp-narrower': 360,
   '--bp-narrow': 500,
+  '--bp-wide': 735,
 };
--- src/type/BreakpointTypes.ts
+++ src/type/BreakpointTypes.ts
@@ -1,6 +1,7 @@
 export interface Breakpoints {
   '--bp-narrower': number;
   '--bp-narrow': number;
+  '--bp-wide': number;
 }
 
 export type BreakpointName = keyof Breakpoints;
